d4757efa48ff66c9f53d71fbb853f1584ef2ef08
じん <j.kumagai.sys22@morijyobi.ac.jp>
2022-11-08 12:21:39 +0900

    javaファイルを変更しました。

--- Main.java
+++ Main.java
@@ -5,9 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double h = sc.nextDouble(); 
         double w = sc.nextDouble(); 
-        double bmi = w / Math.pow(h, 2);
+        double bmi = w / Math.pow(h, 2);        
         double b = ((double)Math.round(bmi * 100))/100;
-		System.out.println(b );
+		System.out.println(b);
+
+        double g = sc.nextDouble(); 
+        double j = sc.nextDouble(); 
+        double bm = j / Math.pow(g, 2);
+        double r = ((double)Math.round(bm * 100))/100;
+		System.out.println(r);
 
     }
 }
